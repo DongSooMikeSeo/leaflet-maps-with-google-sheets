efa5839af5be63fe71890d88754353e888fa09a4
DongSooMikeSeo <61750185+DongSooMikeSeo@users.noreply.github.com>
2025-06-16 17:02:50 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1IRm3VTefmiGh-atJkaHrhn_Id5QfDa-E1n-sjVYnUvI/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyB4lEQCNfVG05heNheLkhEnCeJpEG7t1Fc';
